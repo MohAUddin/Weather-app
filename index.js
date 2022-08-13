7cb194978c42cfb5f402f53c05874e541125ab02
MohAUddin <mohammad.uddin454@gmail.com>
2022-08-13 15:49:47 -0400

    added api call back for city and weather app

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ const wrapper = document.querySelector(".wrapper"),
 inputPart = wrapper.querySelector(".info__part"),
 inputTxt = inputPart.querySelector(".info__part--para"),
 inputField = inputPart.querySelector("input");
+
+inputField.addEventListener("keyup", e =>{
+    // if user pressed enter btn and input value is not empty
+    if(e.key == "Enter" && inputField.value != ""){
+        requestApi(inputField.value);
+    } 
+});
+
+function requestApi(city){
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={092a1ab4dbcdeb96d6d950158c5e6524}`;
+    fetch(api).then(response => console.log(response.json()));
+} 
+
+
