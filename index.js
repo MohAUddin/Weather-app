15b5aaf29c25e95c057744952d88ae16df6a6ec8
MohAUddin <mohammad.uddin454@gmail.com>
2022-08-14 21:00:48 -0400

    added error function for denying geolocation

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ inputField.addEventListener("keyup", e =>{
     } 
 });
 
+locationBtn.addEventListener("click", ()=>{
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(onSuccess, onError)
+    }else{
+        alert("Your browser does not support geolocation API ")
+    }
+});
+
+function onSuccess(postion){
+    console.log(postion);
+}
+
+function onError(error){
+    infoTxt.innerText = error.message;
+    infoTxt.classList.add("error");
+}
+
+
 function requestApi(city){
     let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=092a1ab4dbcdeb96d6d950158c5e6524`;
     infoTxt.innerText = "Getting weather details...";
